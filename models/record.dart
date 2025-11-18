644c35c15e3b9ab8a1d138975eda544a8a2ed85d
mariappanraja2428-commits <mariappanraja2428@gmail.com>
2025-11-18 16:21:47 +0400

    Create Record class with required fields

---
+++ models/record.dart
@@ -0,0 +1,29 @@
+class Record {
+  String area;
+  String line;
+  String size;
+  String material;
+  String ohlCableMfp;
+  String meter;
+  String feeder;
+  String txNo;
+  String place;
+  String location;
+  String beforePhotoPath;
+  String afterPhotoPath;
+
+  Record({
+    required this.area,
+    required this.line,
+    required this.size,
+    required this.material,
+    required this.ohlCableMfp,
+    required this.meter,
+    required this.feeder,
+    required this.txNo,
+    required this.place,
+    required this.location,
+    required this.beforePhotoPath,
+    required this.afterPhotoPath,
+  });
+}
